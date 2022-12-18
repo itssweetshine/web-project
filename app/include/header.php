7cd143cb8309c8bdf59b8eef84b5de3bb52b3c09
aigeraaa <100575053+itssweetshine@users.noreply.github.com>
2022-12-19 05:24:48 +0600

    Create header.php

---
+++ app/include/header.php
@@ -0,0 +1,21 @@
+<div class="head">
+    <div>
+        <img class="logo" src="assets/logo_redblack.png">
+    </div>
+    <div class="dostavka">
+        Доставка Пиццы  <span style="color: white;"> Алматы </span><br>31 мин | 4.78 <img style="width: 20px;height: 20px" src="assets/star.png">
+    </div>
+    <div>
+        <nav>
+            <ul>
+                <li><a class="category" href="home_page.php">Главная страница</a></li>
+                <li><a class="category" href="pizza_page.php">Пицца</a></li>
+                <li><a class="category" href="">Комбо</a></li>
+                <li><a class="category" href="">Закуски</a></li>
+                <li><a class="category" href="dessert_page.php">Десерты</a></li>
+                <li><a class="category" href="drinks_page.php">Напитки</a></li>
+            </ul>
+        </nav>
+    </div>
+    <button type="button" value="basket" class="basket">Корзина</button>
+</div>
