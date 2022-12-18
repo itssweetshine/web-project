d134089878482ca1ef8c606eec70a57ec0353ba1
aigeraaa <100575053+itssweetshine@users.noreply.github.com>
2022-12-18 18:09:12 +0600

    Update and rename home_page.html to home_page.php

---
+++ home_page.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>MaAiTe |Pizzeria|</title>
+    <link rel="icon" type="text/css" href="assets/icon.png">
+    <link rel="stylesheet" href="home_page.css">
+</head>
+<body>
+
+<?php include("app/include/header.php"); ?>
+
+
+<div class="space"></div>
+<div class="main">
+    <div style="display: flex;" >
+        <div class="sale">
+            <img class="aksia" src="assets/акция.jpeg">
+
+        <div class="text-aksia">
+            <h3>День рождения!</h3>
+            <p style="color: #5C6370">В день вашего рождения дарим Пепперони 25 см! Акция действует по промокоду D120 один раз в день только на доставку и самовывоз при достижении минимальной суммы заказа, в течение 14 дней: три дня до, в день рождения и десять дней после него. Укажите день вашего рождения в Личном кабинете. Будьте внимательны, далее изменение даты дня рождения невозможно.</p>
+        </div>
+        </div>
+
+        <div class="sale">
+        <img class="aksia" src="assets/aksia1.jpeg">
+
+        <div class="text-aksia">
+            <h3>Бизнес-ланч</h3>
+            <p style="color: #5C6370">Вкусный и быстрый обед не выходя из офиса! С понедельника по четверг с 11:00 до 14:00 вы можете заказать 2 любые пиццы среднего размера (30 см) всего за 3765 тенге! Доставка всегда бесплатно! *Акция не действует в выходные, праздничные и предпраздничные дни. Промо-код - D100</p>
+        </div>
+        </div>
+
+        <div class="sale">
+            <img class="aksia" src="assets/aksia3.jpeg">
+
+            <div class="text-aksia">
+                <h3>Бизнес-ланч</h3>
+                <p style="color: #5C6370">Вкусный и быстрый обед не выходя из офиса! С понедельника по четверг с 11:00 до 14:00 вы можете заказать 2 любые пиццы среднего размера (30 см) всего за 3765 тенге! Доставка всегда бесплатно! *Акция не действует в выходные, праздничные и предпраздничные дни. Промо-код - D100</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="space"></div>
+
+<?php include("app/include/footer.php"); ?>
+
+<script src="home_page.js"></script>
+</body>
+</html>
