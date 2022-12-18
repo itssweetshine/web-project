156ec82cb48310adf97da513dcedc71e996b5642
aigeraaa <100575053+itssweetshine@users.noreply.github.com>
2022-12-19 05:25:26 +0600

    Create footer.php

---
+++ app/include/footer.php
@@ -0,0 +1,6 @@
+<div class="head">
+    <div class="space"></div>
+    <div class="end anonym"><img style="height: 60px;width: 60px" src="assets/anonymous.png">Стань
+        <a href="anketa.php" class="anketa" type="button" value="анкета">Заполнить анкету</a>
+    </div>
+</div>
