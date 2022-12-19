c4703f1cb9a32233aebc0b6897be8fd3c51077b9
aigeraaa <100575053+itssweetshine@users.noreply.github.com>
2022-12-20 03:32:56 +0600

    Update header.php

--- app/include/header.php
+++ app/include/header.php
@@ -3,19 +3,20 @@
         <img class="logo" src="assets/logo_redblack.png">
     </div>
     <div class="dostavka">
-        Доставка Пиццы  <span style="color: white;"> Алматы </span><br>31 мин | 4.78 <img style="width: 20px;height: 20px" src="assets/star.png">
+        Доставка Пиццы  <span style="color: white;"> Алматы </span><br>31 мин | 4.78 <img style="width: 1.25rem;height: 1.25rem" src="assets/star.png">
     </div>
     <div>
         <nav>
             <ul>
                 <li><a class="category" href="home_page.php">Главная страница</a></li>
                 <li><a class="category" href="pizza_page.php">Пицца</a></li>
-                <li><a class="category" href="">Комбо</a></li>
-                <li><a class="category" href="">Закуски</a></li>
+                <li><a class="category" href="combo.php">Комбо</a></li>
+                <li><a class="category" href="snacks.php">Закуски</a></li>
                 <li><a class="category" href="dessert_page.php">Десерты</a></li>
                 <li><a class="category" href="drinks_page.php">Напитки</a></li>
+<!--                <li><a class="category" href="basket.php"><button><img src="assets/basket.png">| 1</button></li>-->
             </ul>
         </nav>
     </div>
-    <button type="button" value="basket" class="basket">Корзина</button>
+    <a href="login.php" <button type="button" value="basket" class="basket">Войти</button></a>
 </div>
