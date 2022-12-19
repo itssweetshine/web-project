10bf582d36b838486b804285d18b14a9517ebe98
aigeraaa <100575053+itssweetshine@users.noreply.github.com>
2022-12-19 11:20:25 +0600

    Update home_page.php

--- home_page.php
+++ home_page.php
@@ -12,39 +12,71 @@
 
 <?php include("app/include/header.php"); ?>
 
+<!--slideshow start-->
+<!--slideshow end-->
+<div class="grid-container">
+    <div></div>
+    <div class="main">
+        <br><h1>Акции</h1>
+        <div style="display: flex">
+            <div class="sale">
+                <img class="aksia" src="assets/акция.jpeg">
 
-<div class="space"></div>
-<div class="main">
-    <div style="display: flex;" >
-        <div class="sale">
-            <img class="aksia" src="assets/акция.jpeg">
+                <div class="text-aksia"><br>
+                    <h3>День рождения!</h3>
+                    <p style="color: #5C6370">В день вашего рождения дарим Пепперони 25 см! Акция действует по промокоду D120 один раз в день только на доставку и самовывоз при достижении минимальной суммы заказа, в течение 14 дней: три дня до, в день рождения и десять дней после него. Укажите день вашего рождения в Личном кабинете. Будьте внимательны, далее изменение даты дня рождения невозможно.</p>
+                </div>
+            </div>
 
-        <div class="text-aksia">
-            <h3>День рождения!</h3>
-            <p style="color: #5C6370">В день вашего рождения дарим Пепперони 25 см! Акция действует по промокоду D120 один раз в день только на доставку и самовывоз при достижении минимальной суммы заказа, в течение 14 дней: три дня до, в день рождения и десять дней после него. Укажите день вашего рождения в Личном кабинете. Будьте внимательны, далее изменение даты дня рождения невозможно.</p>
+            <div class="sale">
+                <img class="aksia" src="assets/aksia3.jpeg">
+
+                <div class="text-aksia"><br>
+                    <h3>Сырный бортик</h3>
+                    <p style="color: #5C6370">Сыра много не бывает. Добавьте в бортик яркий сыр чеддер и наслаждайтесь любимой пиццей до последней крошки</p>
+                </div>
+            </div>
+
+            <div class="sale">
+                <img class="aksia" src="assets/aksia1.jpeg">
+
+                <div class="text-aksia"><br>
+                    <h3>Бизнес-ланч</h3>
+                    <p style="color: #5C6370">Вкусный и быстрый обед не выходя из офиса! С понедельника по четверг с 11:00 до 14:00 вы можете заказать 2 любые пиццы среднего размера (30 см) всего за 3765 тенге! Доставка всегда бесплатно! *Акция не действует в выходные, праздничные и предпраздничные дни. Промо-код - D100</p>
+                </div>
+            </div>
         </div>
-        </div>
+    </div>
+    <div></div>
+    <div></div>
+    <div class="main">
+        <br><h1>Часто заказывают</h1>
+        <div style="display: flex">
+            <div class="chasto-zakaz">
+                <div class="box-of-chasto-zakaz">
+                    <img class="photo-of-pizza" src="https://dodopizza-a.akamaihd.net/static/Img/Products/405cd1308f3c4c028e32e30885d78863_233x233.webp">
+                    <div class="indeika">Индейка с клюквенным соусом <br><br>2700тг</div>
+                </div>
 
-        <div class="sale">
-        <img class="aksia" src="assets/aksia1.jpeg">
+                <div class="box-of-chasto-zakaz">
+                    <img class="photo-of-pizza" src="https://dodopizza-a.akamaihd.net/static/Img/ComboTemplates/f382490b2d234067b9ba70a768d2f3fd_233x233.webp">
+                    <div class="indeika">4 Закуски <br><br><br>4 000тг</div>
+                </div>
 
-        <div class="text-aksia">
-            <h3>Бизнес-ланч</h3>
-            <p style="color: #5C6370">Вкусный и быстрый обед не выходя из офиса! С понедельника по четверг с 11:00 до 14:00 вы можете заказать 2 любые пиццы среднего размера (30 см) всего за 3765 тенге! Доставка всегда бесплатно! *Акция не действует в выходные, праздничные и предпраздничные дни. Промо-код - D100</p>
-        </div>
-        </div>
+                <div class="box-of-chasto-zakaz">
+                    <img class="photo-of-pizza" src="https://dodopizza-a.akamaihd.net/static/Img/Products/8fb4c81d102b4f16afe584f703ac5e5b_233x233.webp">
+                    <div class="indeika">Супермясной Додстер<br><br><br>1 500тг</div>
+                </div>
 
-        <div class="sale">
-            <img class="aksia" src="assets/aksia3.jpeg">
-
-            <div class="text-aksia">
-                <h3>Бизнес-ланч</h3>
-                <p style="color: #5C6370">Вкусный и быстрый обед не выходя из офиса! С понедельника по четверг с 11:00 до 14:00 вы можете заказать 2 любые пиццы среднего размера (30 см) всего за 3765 тенге! Доставка всегда бесплатно! *Акция не действует в выходные, праздничные и предпраздничные дни. Промо-код - D100</p>
+                <div class="box-of-chasto-zakaz">
+                    <img class="photo-of-pizza" src="https://dodopizza-a.akamaihd.net/static/Img/Products/50915bf616ce4a498f5b42830d1118d7_233x233.webp">
+                    <div class="indeika">Манго-шейк <br><br><br>1 800тг</div>
+                </div>
             </div>
         </div>
     </div>
 </div>
-<div class="space"></div>
+
 
 <?php include("app/include/footer.php"); ?>
 
