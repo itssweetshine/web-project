7f89e85c08cb325765692b860f5531df60751e18
aigeraaa <100575053+itssweetshine@users.noreply.github.com>
2022-12-20 03:07:19 +0600

    Update footer.php

--- app/include/footer.php
+++ app/include/footer.php
@@ -1,6 +1,29 @@
-<div class="head">
-    <div class="space"></div>
-    <div class="end anonym"><img style="height: 60px;width: 60px" src="assets/anonymous.png">Стань
-        <a href="anketa.php" class="anketa" type="button" value="анкета">Заполнить анкету</a>
+<div class="footer">
+    <div class="place-footera">
+        <div class="O-nas">
+            <p class="zagalovki">О нас</p>
+            <p style="color: #B9B9B9;">Легендарная пицца MaAiTe — блюдо, завоевавшее любовь тысяч людей. Ее покупают на пикники, на обеды в офисы, домой для дружеских посиделок с друзьями или для романтических ужинов.</p>
+        </div>
+
+        <div class="Kontakti">
+            <p class="zagalovki">Контакты</p>
+            <div class="row-v-kontaktah">
+                <p style="color: #B9B9B9;"><img class="ikonka-v-kontaktah" src="https://png.pngtree.com/png-clipart/20190630/original/pngtree-vector-house-icon-png-image_4162861.jpg">Oфис:г.Каскелен ,123 кв</p>
+            </div>
+            <div class="row-v-kontaktah">
+                <p style="color: #B9B9B9;"><img class="ikonka-v-kontaktah" src="https://png.pngtree.com/png-clipart/20190516/original/pngtree-vector-email-icon-png-image_4141379.jpg">Почта:MaAiTe@mail.ru</p>
+            </div>
+            <div class="row-v-kontaktah">
+                <p style="color: #B9B9B9;"><img class="ikonka-v-kontaktah" src="https://png.pngtree.com/png-clipart/20190925/original/pngtree-phone-icon-for-your-project-png-image_4891511.jpg">По Казахстану:+7(777)-707-70-07</p>
+            </div>
+        </div>
+
+        <div class="Kontakti">
+            <img class="ikonka-mess" src="whatsapp.png">
+            <img class="ikonka-mess" src="https://cdn-icons-png.flaticon.com/512/174/174848.png">
+            <img class="ikonka-mess" src="https://pngicon.ru/file/uploads/instagram.png">
+            <img class="download" src="appstore.png">
+            <img class="download" src="market.png">
+        </div>
     </div>
 </div>
