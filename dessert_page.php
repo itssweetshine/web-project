81f6c90a57c7ea7ed516be77c589cf9b6d13b439
aigeraaa <100575053+itssweetshine@users.noreply.github.com>
2022-12-19 11:19:50 +0600

    Update dessert_page.php

--- dessert_page.php
+++ dessert_page.php
@@ -10,117 +10,118 @@
 <body>
 
 <?php include("app/include/header.php"); ?>
+<div class="grid-container">
+    <div></div>
+    <div class="main">
+        <br><h1>Десерты</h1>
+        <div style="display: flex">
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/50915bf616ce4a498f5b42830d1118d7_366x366.webp">
+                <h3>Манго-шейк</h3>
+                <p style="color: #5C6370; height: 120px">Жаркое тропическое лето в стакане — молочный коктейль с манговым пюре</p>
+                <div><b>1800 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/cad8ca6c44bc467992ef5a4735e7ce92_366x366.webp">
+                <h3>Айс Капучино на доставку (NEW)</h3>
+                <p style="color: #5C6370; height: 100px">Холодный кофейный напиток теперь со сливочным вкусом. В составе мороженое, эспрессо и молоко</p>
+                <div><b>1300 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/818a6c1743de412fbfe4fa6c533d647b_366x366.webp">
+                <h3>Молочный коктейль с печеньем Орео</h3>
+                <p style="color: #5C6370; height: 100px">Напиток из молока и мороженого с добавлением дробленого печенья «Орео»</p>
+                <div><b>1500 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/81ad496ccabd437b8ebd8b285758e34e_366x366.webp">
+                <h3>Классический молочный коктейль</h3>
+                <p style="color: #5C6370; height: 100px">Напиток из молока и мороженого</p>
+                <div><b>1400 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+        </div>
 
-<div class="space"></div>
-<div class="main">
-  <br><h1>Десерты</h1>
+        <div style="display: flex">
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/761f71c4f286484c850642ed28d690c0_366x366.webp">
+                <h3>Клубничный молочный коктейль</h3>
+                <p style="color: #5C6370; height: 100px">Напиток из молока и мороженого с клубничным сиропом</p>
+                <div><b>1400 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/66ab678966b648bb91774dd128c6254e_366x366.webp">
+                <h3>Слоеные палочки с ананасами и брусникой</h3>
+                <p style="color: #5C6370; height: 100px">Здесь все сразу — брусника и ананас со сгущенкой на слоеном тесте</p>
+                <div><b>1600тг.</b><button class="btn">В корзину</button></div>
+            </div>
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/8fd9d9d2e59542979a4f0e6617764c7d_366x366.webp">
+                <h3>Слоеные палочки с брусникой</h3>
+                <p style="color: #5C6370; height: 100px">Звучит ягодно! Спелая брусника со сгущенкой на слоеном тесте</p>
+                <div><b>1600 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/6d890320abbc423f80fed4ff314fdb08_366x366.webp">
+                <h3>Чизкейк Банановый с шоколадным печеньем</h3>
+                <p style="color: #5C6370; height: 100px">Солнечный снаружи и яркий по вкусу внутри. Летняя новинка — нежный чизкейк с бананом и шоколадным печеньем</p>
+                <div><b>1400 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+        </div>
 
-  <div style="display: flex">
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/50915bf616ce4a498f5b42830d1118d7_366x366.webp">
-      <h3>Манго-шейк</h3>
-      <p style="color: #5C6370; height: 120px">Жаркое тропическое лето в стакане — молочный коктейль с манговым пюре</p>
-      <div>1800 тг.<button class="btn">В корзину</button></div>
-    </div>
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/cad8ca6c44bc467992ef5a4735e7ce92_366x366.webp">
-      <h3>Айс Капучино на доставку (NEW)</h3>
-      <p style="color: #5C6370; height: 100px">Холодный кофейный напиток теперь со сливочным вкусом. В составе мороженое, эспрессо и молоко</p>
-      <div>1300 тг.<button class="btn">В корзину</button></div>
-    </div>
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/818a6c1743de412fbfe4fa6c533d647b_366x366.webp">
-      <h3>Молочный коктейль с печеньем Орео</h3>
-      <p style="color: #5C6370; height: 100px">Напиток из молока и мороженого с добавлением дробленого печенья «Орео»</p>
-      <div>1500 тг.<button class="btn">В корзину</button></div>
-    </div>
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/81ad496ccabd437b8ebd8b285758e34e_366x366.webp">
-      <h3>Классический молочный коктейль</h3>
-      <p style="color: #5C6370; height: 100px">Напиток из молока и мороженого</p>
-      <div>1400 тг.<button class="btn">В корзину</button></div>
-    </div>
-  </div>
+        <div style="display: flex">
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/77e29054d9cf44db9d336c46c57522b8_366x366.webp">
+                <h3>Чизкейк Нью-Йорк</h3>
+                <p style="color: #5C6370; height: 100px">Мы перепробовали тысячу десертов и наконец нашли любимца гостей — нежнейший творожный чизкейк</p>
+                <div><b>900 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/60074739962b48108864a884b3ed465c_366x366.webp">
+                <h3>Фондан</h3>
+                <p style="color: #5C6370; height: 100px">Четверо из пяти гостей говорят «Oh la la!», когда едят этот французский десерт с хрустящей корочкой и топленой шоколадной начинкой</p>
+                <div><b>1600 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/813fad76340b444fad592fa8467dbdbf_366x366.webp">
+                <h3>Рулетики с брусникой</h3>
+                <p style="color: #5C6370; height: 100px">Это задорные сладкие рулетики, в которых закрутился микс из натуральной брусники и сгущенного молока</p>
+                <div><b>1000 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/41d5d551787941e29684ab98eca39205_366x366.webp">
+                <h3>Рулетики с корицей</h3>
+                <p style="color: #5C6370; height: 100px">Десерт, одобренный нашими бабушками. Горячие сладкие рулетики с пряной корицей и сахаром</p>
+                <div><b>900 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+        </div>
 
-  <div style="display: flex">
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/761f71c4f286484c850642ed28d690c0_366x366.webp">
-      <h3>Клубничный молочный коктейль</h3>
-      <p style="color: #5C6370; height: 100px">Напиток из молока и мороженого с клубничным сиропом</p>
-      <div>1400 тг.<button class="btn">В корзину</button></div>
+        <div style="display: flex">
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/4953983c99f945108d3e5b9e5a901476_366x366.webp">
+                <h3>Сырники с малиновым вареньем</h3>
+                <p style="color: #5C6370; height: 100px">Любимый десерт многих наших гостей — румяные сырники из печи. Такие нежные, в меру сладкие и напоминающие детство</p>
+                <div><b>1600 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/02641ff61b4c4fc8a6be133323aef06a_366x366.webp">
+                <h3>Маффин Соленая карамель</h3>
+                <p style="color: #5C6370; height: 100px">Раз откусить — навсегда полюбить! Оцените яркое сочетание соленой карамели и арахиса</p>
+                <div><b>700 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+            <div class="cards">
+                <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/d91da9e96e9448e6978d2e6ce60c3e84_366x366.webp">
+                <h3>Шоколадный кукис</h3>
+                <p style="color: #5C6370; height: 120px">На вид печенье как планета, а на вкус — шоколадная комета с глазурью</p>
+                <div><b>600 тг.</b><button class="btn">В корзину</button></div>
+            </div>
+            <div class="cards"></div>
+        </div>
     </div>
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/66ab678966b648bb91774dd128c6254e_366x366.webp">
-      <h3>Слоеные палочки с ананасами и брусникой</h3>
-      <p style="color: #5C6370; height: 100px">Здесь все сразу — брусника и ананас со сгущенкой на слоеном тесте</p>
-      <div>1600тг.<button class="btn">В корзину</button></div>
-    </div>
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/8fd9d9d2e59542979a4f0e6617764c7d_366x366.webp">
-      <h3>Слоеные палочки с брусникой</h3>
-      <p style="color: #5C6370; height: 100px">Звучит ягодно! Спелая брусника со сгущенкой на слоеном тесте</p>
-      <div>1600 тг.<button class="btn">В корзину</button></div>
-    </div>
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/6d890320abbc423f80fed4ff314fdb08_366x366.webp">
-      <h3>Чизкейк Банановый с шоколадным печеньем</h3>
-      <p style="color: #5C6370; height: 100px">Солнечный снаружи и яркий по вкусу внутри. Летняя новинка — нежный чизкейк с бананом и шоколадным печеньем</p>
-      <div>1400 тг.<button class="btn">В корзину</button></div>
-    </div>
-  </div>
-
-  <div style="display: flex">
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/77e29054d9cf44db9d336c46c57522b8_366x366.webp">
-      <h3>Чизкейк Нью-Йорк</h3>
-      <p style="color: #5C6370; height: 100px">Мы перепробовали тысячу десертов и наконец нашли любимца гостей — нежнейший творожный чизкейк</p>
-      <div>900 тг.<button class="btn">В корзину</button></div>
-    </div>
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/60074739962b48108864a884b3ed465c_366x366.webp">
-      <h3>Фондан</h3>
-      <p style="color: #5C6370; height: 100px">Четверо из пяти гостей говорят «Oh la la!», когда едят этот французский десерт с хрустящей корочкой и топленой шоколадной начинкой</p>
-      <div>1600 тг.<button class="btn">В корзину</button></div>
-    </div>
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/813fad76340b444fad592fa8467dbdbf_366x366.webp">
-      <h3>Рулетики с брусникой</h3>
-      <p style="color: #5C6370; height: 100px">Это задорные сладкие рулетики, в которых закрутился микс из натуральной брусники и сгущенного молока</p>
-      <div>1000 тг.<button class="btn">В корзину</button></div>
-    </div>
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/41d5d551787941e29684ab98eca39205_366x366.webp">
-      <h3>Рулетики с корицей</h3>
-      <p style="color: #5C6370; height: 100px">Десерт, одобренный нашими бабушками. Горячие сладкие рулетики с пряной корицей и сахаром</p>
-      <div>900 тг.<button class="btn">В корзину</button></div>
-    </div>
-  </div>
-
-  <div style="display: flex">
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/4953983c99f945108d3e5b9e5a901476_366x366.webp">
-      <h3>Сырники с малиновым вареньем</h3>
-      <p style="color: #5C6370; height: 100px">Любимый десерт многих наших гостей — румяные сырники из печи. Такие нежные, в меру сладкие и напоминающие детство</p>
-      <div>1600 тг.<button class="btn">В корзину</button></div>
-    </div>
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/02641ff61b4c4fc8a6be133323aef06a_366x366.webp">
-      <h3>Маффин Соленая карамель</h3>
-      <p style="color: #5C6370; height: 100px">Раз откусить — навсегда полюбить! Оцените яркое сочетание соленой карамели и арахиса</p>
-      <div>700 тг.<button class="btn">В корзину</button></div>
-    </div>
-    <div class="cards">
-      <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/d91da9e96e9448e6978d2e6ce60c3e84_366x366.webp">
-      <h3>Шоколадный кукис</h3>
-      <p style="color: #5C6370; height: 120px">На вид печенье как планета, а на вкус — шоколадная комета с глазурью</p>
-      <div>600 тг.<button class="btn">В корзину</button></div>
-    </div>
-    <div class="cards"></div>
-  </div>
+    <div></div>
 </div>
-<div class="space"></div>
 
-<?php include("app/include/header.php"); ?>
+
+<?php include("app/include/footer.php"); ?>
 
 </body>
 </html>
