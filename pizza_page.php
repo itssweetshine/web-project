3c319aa2a6330f19b6406c4f43197e84cd20ca6d
aigeraaa <100575053+itssweetshine@users.noreply.github.com>
2022-12-20 09:59:43 +0600

    Update pizza_page.php

--- pizza_page.php
+++ pizza_page.php
@@ -19,25 +19,25 @@
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/89211781fc6f47c1b74b5ca72569e236_366x366.webp">
                 <h3>Индейка с клюквенным соусом</h3>
-                <p style="color: #5C6370; height: 100px">Индейка, клюквенный соус, брусника, красный лук, моцарелла, соус альфредо</p>
+                <p class="mini-text">Индейка, клюквенный соус, брусника, красный лук, моцарелла, соус альфредо</p>
                 <div><b>2900 тг.</b><button class="btn pop-up-button">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/af34ab8410eb4b5d9b996af6d03709bd_366x366.webp">
                 <h3>Цыпленок карри</h3>
-                <p style="color: #5C6370; height: 100px">Цыпленок, ананасы, соус карри, красный лук, сладкий перец, моцарелла, томатный соус</p>
+                <p class="mini-text">Цыпленок, ананасы, соус карри, красный лук, сладкий перец, моцарелла, томатный соус</p>
                 <br><div><b>2800 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/8821b415470a402bb08fb9ee414f6a5a_366x366.webp">
                 <h3>Индейка с овощами гриль</h3>
-                <p style="color: #5C6370; height: 100px">Пастрами из индейки, овощи-гриль, кубики брынзы, моцарелла, томатный соус</p>
+                <p class="mini-text">Пастрами из индейки, овощи-гриль, кубики брынзы, моцарелла, томатный соус</p>
                 <br><div><b>3600 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/a617adaf92cc41f5be31e298f66eed20_366x366.webp">
                 <h3>Мясной Микс</h3>
-                <p style="color: #5C6370; height: 100px">Пастрами из индейки, острая чоризо из цыпленка, пикантная пепперони из цыпленка, ветчина из цыпленка, моцарелла, томатный соус</p>
+                <p class="mini-text">Пастрами из индейки, острая чоризо из цыпленка, пикантная пепперони из цыпленка, ветчина из цыпленка, моцарелла, томатный соус</p>
                 <br><div><b>2800 тг.</b><button class="btn">В корзину</button></div>
             </div>
         </div>
@@ -46,25 +46,25 @@
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/f9e48bc49c6c421e8894ca581a735bc4_366x366.webp">
                 <h3>Чоризо фреш</h3>
-                <p style="color: #5C6370; height: 100px">Томатный соус, моцарелла, острая чоризо из цыпленка, сладкий перец</p>
+                <p class="mini-text">Томатный соус, моцарелла, острая чоризо из цыпленка, сладкий перец</p>
                 <br><div><b>2600 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/e7ce2397f8d24071bcff0efb1e8db646_366x366.webp">
                 <h3>Домашняя</h3>
-                <p style="color: #5C6370; height: 100px">Пикантная пепперони из цыпленка, ветчина из цыпленка, маринованные огурчики, томаты, моцарелла, томатный соус</p>
+                <p class="mini-text">Пикантная пепперони из цыпленка, ветчина из цыпленка, маринованные огурчики, томаты, моцарелла, томатный соус</p>
                 <br><div><b>2300 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/9f32d6faf1354a35af835143ff52a68f_366x366.webp">
                 <h3>Сырная</h3>
-                <p style="color: #5C6370; height: 100px">Увеличенная порция моцареллы, сыры чеддер и пармезан, соус альфредо</p>
+                <p class="mini-text">Увеличенная порция моцареллы, сыры чеддер и пармезан, соус альфредо</p>
                 <br><div><b>1900 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/b9e9517672f94fd98640017e61d35205_366x366.webp">
                 <h3>Пепперони фреш</h3>
-                <p style="color: #5C6370; height: 100px">Пепперони из цыпленка, увеличенная порция моцареллы, томаты, томатный соус</p>
+                <p class="mini-text">Пепперони из цыпленка, увеличенная порция моцареллы, томаты, томатный соус</p>
                 <br><div><b>3200 тг.</b><button class="btn">В корзину</button></div>
             </div>
         </div>
@@ -73,25 +73,25 @@
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/ceb71e57f8bd4239b08335b6524189b4_366x366.webp">
                 <h3>Овощи и грибы</h3>
-                <p style="color: #5C6370; height: 100px">Шампиньоны, томаты, сладкий перец, красный лук, кубики брынзы, моцарелла, томатный соус, итальянские травы</p>
+                <p class="mini-text">Шампиньоны, томаты, сладкий перец, красный лук, кубики брынзы, моцарелла, томатный соус, итальянские травы</p>
                 <br><div><b>3600 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/bcfeb8fe346c48c2afb8ad5d7e30c73e_366x366.webp">
                 <h3>Песто</h3>
-                <p style="color: #5C6370; height: 100px">Соус песто, соус альфредо, цыплёнок, кубики брынзы, томаты, моцарелла</p>
+                <p class="mini-text">Соус песто, соус альфредо, цыплёнок, кубики брынзы, томаты, моцарелла</p>
                 <br><div><b>2100 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/6b7eb484d92b4bceb64b543012bcc1ef_366x366.webp">
                 <h3>Ветчина и сыр</h3>
-                <p style="color: #5C6370; height: 100px">Ветчина из цыпленка, увеличенная порция моцареллы, соус альфредо</p>
+                <p class="mini-text">Ветчина из цыпленка, увеличенная порция моцареллы, соус альфредо</p>
                 <br><div><b>2500 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/26834bc951fa47d5913a5e169a974a0b_366x366.webp">
                 <h3>Карбонара</h3>
-                <p style="color: #5C6370; height: 100px">Ветчина из цыпленка, сыры чеддер и пармезан, моцарелла, томаты, красный лук, чеснок, соус альфредо, итальянские травы</p>
+                <p class="mini-text">Ветчина из цыпленка, сыры чеддер и пармезан, моцарелла, томаты, красный лук, чеснок, соус альфредо, итальянские травы</p>
                 <br><div><b>3300 тг.</b><button class="btn">В корзину</button></div>
             </div>
         </div>
@@ -100,25 +100,25 @@
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/cbc12c743675470287cf89a6f7cf4208_366x366.webp">
                 <h3>Додо Микс</h3>
-                <p style="color: #5C6370; height: 100px">Цыпленок, ветчина из цыпленка, сыры чеддер и пармезан, соус песто, кубики брынзы, томаты, красный лук, моцарелла, соус альфредо, чеснок, итальянские травы</p>
+                <p class="mini-text">Цыпленок, ветчина из цыпленка, сыры чеддер и пармезан, соус песто, кубики брынзы, томаты, красный лук, моцарелла, соус альфредо, чеснок, итальянские травы</p>
                 <br><div><b>3500 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/d2c4e3df89ae4e42b08a41981de79171_366x366.webp">
                 <h3>Пепперони</h3>
-                <p style="color: #5C6370; height: 100px">Пепперони из цыпленка, увеличенная порция моцареллы, томатный соус</p>
+                <p class="mini-text">Пепперони из цыпленка, увеличенная порция моцареллы, томатный соус</p>
                 <br><div><b>3100 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/a8ae844fcc604bae895d46b2cef9bf9e_366x366.webp">
                 <h3>Маргарита</h3>
-                <p style="color: #5C6370; height: 100px">Увеличенная порция моцареллы, томаты, итальянские травы, томатный соус</p>
+                <p class="mini-text">Увеличенная порция моцареллы, томаты, итальянские травы, томатный соус</p>
                 <br><div><b>2200 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/1c61dd003410437faff53fc741a20166_366x366.webp">
                 <h3>Ветчина и грибы</h3>
-                <p style="color: #5C6370; height: 100px">Ветчина из цыпленка, шампиньоны, моцарелла, томатный соус</p>
+                <p class="mini-text">Ветчина из цыпленка, шампиньоны, моцарелла, томатный соус</p>
                 <br><div><b>2500 тг.</b><button class="btn">В корзину</button></div>
             </div>
         </div>
@@ -127,25 +127,25 @@
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/cadff30e298d47989f76ab99cffa1d97_366x366.webp">
                 <h3>Гавайская</h3>
-                <p style="color: #5C6370; height: 100px">Ветчина из цыпленка, ананасы, моцарелла, томатный соус</p>
+                <p class="mini-text">Ветчина из цыпленка, ананасы, моцарелла, томатный соус</p>
                 <br><div><b>2400 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/fe3272966f3842daaa8fff44a1939cf5_366x366.webp">
                 <h3>Сырный цыплёнок</h3>
-                <p style="color: #5C6370; height: 100px">Цыпленок, моцарелла, сыры чеддер и пармезан, сырный соус, томаты, соус альфредо, чеснок</p>
+                <p class="mini-text">Цыпленок, моцарелла, сыры чеддер и пармезан, сырный соус, томаты, соус альфредо, чеснок</p>
                 <br><div><b>3100 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/c29155baebbc4c6ea3bd069dba97d8e4_366x366.webp">
                 <h3>Цыплёнок ранч</h3>
-                <p style="color: #5C6370; height: 100px">Цыпленок, ветчина из цыпленка, моцарелла, соус ранч, томаты, чеснок</p>
+                <p class="mini-text">Цыпленок, ветчина из цыпленка, моцарелла, соус ранч, томаты, чеснок</p>
                 <br><div><b>3000 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/c06c5695d8f2430993114e32a29da493_366x366.webp">
                 <h3>Аррива!</h3>
-                <p style="color: #5C6370; height: 100px">Соус бургер, цыпленок, соус ранч, чоризо из цыпленка, сладкий перец, красный лук, моцарелла, томаты, чеснок</p>
+                <p class="mini-text">Соус бургер, цыпленок, соус ранч, чоризо из цыпленка, сладкий перец, красный лук, моцарелла, томаты, чеснок</p>
                 <br><div><b>2900 тг.</b><button class="btn">В корзину</button></div>
             </div>
         </div>
@@ -154,25 +154,25 @@
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/1bc70786fe494d1686f613726cc18a70_366x366.webp">
                 <h3>Четыре сезона</h3>
-                <p style="color: #5C6370; height: 100px">Моцарелла, ветчина из цыпленка, пепперони из цыпленка, кубики брынзы, томаты, шампиньоны, томатный соус, итальянские травы</p>
+                <p class="mini-text">Моцарелла, ветчина из цыпленка, пепперони из цыпленка, кубики брынзы, томаты, шампиньоны, томатный соус, итальянские травы</p>
                 <br><div><b>2700 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/ab240304ddef4465889f9b1fe03657a0_366x366.webp">
                 <h3>Чизбургер пицца</h3>
-                <p style="color: #5C6370; height: 100px">Мясной соус болоньезе, моцарелла, красный лук, маринованные огурчики, томаты, соус бургер</p>
+                <p class="mini-text">Мясной соус болоньезе, моцарелла, красный лук, маринованные огурчики, томаты, соус бургер</p>
                 <br><div><b>2600 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/b092327f01b54e5996c0495f3c2ed129_366x366.webp">
                 <h3>Колбаски барбекю</h3>
-                <p style="color: #5C6370; height: 100px">Острая чоризо из цыпленка, соус барбекю, томаты, красный лук, моцарелла, томатный соус</p>
+                <p class="mini-text">Острая чоризо из цыпленка, соус барбекю, томаты, красный лук, моцарелла, томатный соус</p>
                 <br><div><b>2700 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/fdf3b8c6c38d409b850512bd6f136e73_366x366.webp">
                 <h3>Четыре сыра</h3>
-                <p style="color: #5C6370; height: 100px">Моцарелла, сыры чеддер и пармезан, сыр блю чиз, соус альфредо</p>
+                <p class="mini-text">Моцарелла, сыры чеддер и пармезан, сыр блю чиз, соус альфредо</p>
                 <br><div><b>3500 тг.</b><button class="btn">В корзину</button></div>
             </div>
         </div>
@@ -181,25 +181,25 @@
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/5ff8baa047524864810890dc08cd176e_366x366.webp">
                 <h3>Цыплёнок барбекю</h3>
-                <p style="color: #5C6370; height: 100px">Цыпленок, соус барбекю, томатный соус, красный лук, моцарелла, ветчина из цыпленка</p>
+                <p class="mini-text">Цыпленок, соус барбекю, томатный соус, красный лук, моцарелла, ветчина из цыпленка</p>
                 <br><div><b>3200 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/ce0b5a2f4c1c4b75a45a9207a9322216_366x366.webp">
                 <h3>Двойная пепперони</h3>
-                <p style="color: #5C6370; height: 100px">Пепперони из цыпленка, увеличенная порция моцареллы, томатный соус</p>
+                <p class="mini-text">Пепперони из цыпленка, увеличенная порция моцареллы, томатный соус</p>
                 <br><div><b>2300 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/afccc45d4d5e40fda6d1a8a3de31f6eb_366x366.webp">
                 <h3>Диабло</h3>
-                <p style="color: #5C6370; height: 100px">Острая чоризо из цыпленка, острый перец халапеньо, соус барбекю, митболы из говядины, томаты, сладкий перец, красный лук, моцарелла, томатный соус</p>
+                <p class="mini-text">Острая чоризо из цыпленка, острый перец халапеньо, соус барбекю, митболы из говядины, томаты, сладкий перец, красный лук, моцарелла, томатный соус</p>
                 <br><div><b>2600 тг.</b><button class="btn">В корзину</button></div>
             </div>
             <div class="cards">
                 <img class="products" src="https://dodopizza-a.akamaihd.net/static/Img/Products/139805ff0ae0442caf019591abf5d466_366x366.webp">
                 <h3>Додо</h3>
-                <p style="color: #5C6370; height: 100px">Ветчина из цыпленка, митболы из говядины, пепперони из цыпленка, моцарелла, томаты, шампиньоны, сладкий перец, красный лук, чеснок, томатный соус</p>
+                <p class="mini-text">Ветчина из цыпленка, митболы из говядины, пепперони из цыпленка, моцарелла, томаты, шампиньоны, сладкий перец, красный лук, чеснок, томатный соус</p>
                 <br><div><b>2800 тг.</b><button class="btn">В корзину</button></div>
             </div>
         </div>
@@ -209,6 +209,6 @@
 
 
 <?php include("app/include/footer.php"); ?>
-
+<script src="home_page.php"></script>
 </body>
 </html>
